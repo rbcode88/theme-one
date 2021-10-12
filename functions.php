d14b4f759eeefd6095e4bb902a1c80acf848ed6b
rbcode88 <92356529+rbcode88@users.noreply.github.com>
2021-10-12 10:51:46 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * Enqueue scripts and styles
+ */
+
+function trophy_theme_one() {
+    // styles
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array() );
+    wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/style.css', array() );
+    // scripts
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/scripts.js', array('jquery'), '', true );
+ }
+add_action( 'wp_enqueue_scripts', 'trophy_theme_one' );
+
+
+add_theme_support('post-thumbnails');
+add_post_type_support( 'nagrody', 'thumbnail' );
+
+
+function nagrody() {
+    register_post_type('nagrody',
+        array(
+            'labels'      => array(
+                'name'          => __( 'Nagrody', 'nagrody' ),
+                'singular_name' => __( 'Nagroda', 'nagrody' ),
+                'add_new' => __( 'Dodaj' ),
+                'add_new_item' => __( 'Dodaj nową nagrodę' )
+                ),
+            'taxonomies'  => array( 'Genre' ),
+            'query_var' => true,
+            'public'  => true,
+            'has_archive' => true,
+            'show_ui' => true,
+            'menu_icon' => 'dashicons-star-filled',
+            'rewrite' => array(
+            'slug'  => 'nagrody'
+            )
+        )
+    );
+}
+add_action('init', 'nagrody');
+
+function trophy_category() {
+    register_taxonomy( 'Kategoria', 'nagrody', array(
+        'label'        => __( 'Kategoria', 'nagrody' ),
+        'rewrite'      => array( 'slug' => 'book-genre' ),
+        'show_admin_column' => true,
+        'hierarchical' => true,
+    ) );
+}
+add_action( 'init', 'trophy_category', 0 );
+
+
+function most_recent_book_title() {
+
+  $args = array(
+      'post_type' => 'Nagrody',
+      'posts_per_page' => 1,
+    );
+    $loop = new WP_Query( $args );
+
+    if ( $loop->have_posts() ) { ?>
+      <?php while ( $loop->have_posts() ) : $loop->the_post();
+
+      $title = the_title();
+
+      endwhile;
+
+      wp_reset_query();
+    }
+
+  return $title;
+}
+
+ add_shortcode('recent-book', 'most_recent_book_title');
+
+function last_five_genre_books($atts) {
+
+$arg =  shortcode_atts( array(
+          'id' => 1,
+      ), $atts);
+
+$termId = $arg['id'];
+
+      $args = array(
+          'orderby'=> 'name',
+          'order' => 'ASC',
+          'post_type' => 'Nagrody',
+          'posts_per_page' => 5,
+          'tax_query' => array(
+           array (
+             'taxonomy' => 'Genre',
+             'field' => 'name',
+             'terms' => $termId,
+             )
+           ),
+      );
+
+      $my_query = new WP_Query($args); ?>
+
+      <ol>
+
+      <?php if( $my_query->have_posts() ) {
+          while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+          <li><?php $message = the_title(); ?> </li>
+
+      <?php endwhile;
+
+       } ?>
+
+       </ol>
+
+       <?php wp_reset_query();
+
+       return $message;
+
+}
+
+add_shortcode('genre-books', 'last_five_genre_books');
+
+
+function number_posts_per_page( $query ) {
+
+  global $wp_the_query;
+
+  if ($query->is_archive() ) {
+    $query->set( 'posts_per_page', 5 );
+  }
+  else {
+    $query->set( 'posts_per_page', 9 );
+  }
+
+  return $query;
+}
+add_action( 'pre_get_posts',  'number_posts_per_page'  );
+
+// function ajax_enqueue() {
+//   wp_enqueue_script( 'ajax-script', plugins_url( '/assets/js/script.js', __FILE__ ), array('jquery') );
+//   wp_localize_script( 'ajax-script', 'get_ajax_books', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+// }
+// add_action( 'wp_enqueue_scripts', 'ajax_enqueue' );
+
+// function ajax_posts() {
+//     $args = array(
+//         'post_type' => 'Bagrody',
+//         'posts_per_page' => 20,
+//     );
+
+//     $ajaxposts = get_posts( $args );
+
+//     $list = array();
+
+//         foreach ( $ajaxposts as $post ) {
+
+//            $terms = get_the_terms( $post , 'Genre' );
+
+//            foreach ( $terms as $term ) {
+//              $list[] = array(
+//                'name'   => $post->post_title,
+//                'date' => $post->post_date,
+//                'genre' => $term->name,
+//                'excerpt' =>$post->post_excerpt
+//              );
+//            }
+
+//         }
+//       echo json_encode( $list );
+
+//     exit;
+// }
+
+// add_action('wp_ajax_ajax_posts', 'ajax_posts');
+// add_action('wp_ajax_nopriv_ajax_posts', 'ajax_posts');
---
+++ header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
