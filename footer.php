319dbc81a78004fc6be10e0d820c7d635ff23c4b
robson1988 <robert.bajoo@gmail.com>
2021-10-25 20:03:52 +0200

    #dom

--- footer.php
+++ footer.php
@@ -14,6 +14,18 @@
             <h6 class="text-uppercase mb-4 font-weight-bold">
               Company name
             </h6>
+            <?php 
+           if(function_exists('the_custom_logo')){ //Page logo 
+             $custom_logo_id = get_theme_mod('custom_logo');
+             $custom_logo_alt =  get_post_meta($custom_logo_id, '_wp_attachment_image_alt', true);
+             $logo = wp_get_attachment_image_src($custom_logo_id);
+
+             if($logo == true){
+             echo "<img class='img-fluid img-logo' id='logo' src=" . $logo[0] . " alt=" . $custom_logo_alt . ">";
+             }
+           }
+          
+          ?> 
             <p>
               Here you can use rows and columns to organize your footer
               content. Lorem ipsum dolor sit amet, consectetur adipisicing
@@ -24,113 +36,80 @@
 
           <hr class="w-100 clearfix d-md-none" />
 
-          <!-- Grid column -->
+          <!--== Footer menu column ==-->
           <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-            <p>
-              <a class="text-white">MDBootstrap</a>
-            </p>
-            <p>
-              <a class="text-white">MDWordPress</a>
-            </p>
-            <p>
-              <a class="text-white">BrandFlow</a>
-            </p>
-            <p>
-              <a class="text-white">Bootstrap Angular</a>
-            </p>
+            <h6 class="text-uppercase mb-4 font-weight-bold">Menu</h6>
+
+            <?php
+               wp_nav_menu(array(
+                'theme_location' => 'footer',
+                'container' => false,
+                'menu_class' => '',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                'depth' => 2,
+                'walker' => new bootstrap_5_wp_nav_menu_walker()
+            ));
+            ?>
+            
           </div>
-          <!-- Grid column -->
+          <!--== Footer menu column end ==-->
 
           <hr class="w-100 clearfix d-md-none" />
 
           <!-- Grid column -->
           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">
-              Useful links
-            </h6>
+            <h6 class="text-uppercase mb-4 font-weight-bold"></h6>
             <p>
-              <a class="text-white">Your Account</a>
+              <a class="white-custom-text">Your Account</a>
             </p>
             <p>
-              <a class="text-white">Become an Affiliate</a>
+              <a class="white-custom-text">Become an Affiliate</a>
             </p>
             <p>
-              <a class="text-white">Shipping Rates</a>
+              <a class="white-custom-text">Shipping Rates</a>
             </p>
             <p>
-              <a class="text-white">Help</a>
+              <a class="white-custom-text">Help</a>
             </p>
           </div>
 
           <!-- Grid column -->
           <hr class="w-100 clearfix d-md-none" />
 
-          <!-- Grid column -->
+          <!--=== Contact column ===-->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+            <h6 class="text-uppercase mb-4 font-weight-bold">Kontakt</h6>
+            <p><i class="fas fa-home mr-3"></i> Grabskiego 8, 37-450 Stalowa Wola</p>
             <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
             <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
           </div>
-          <!-- Grid column -->
+          <!--=== Contact column end===-->
         </div>
         <!--Grid row-->
       </section>
       <!-- Section: Links -->
 
-      <hr class="my-3">
+      <hr class="my-3 hr-line">
 
       <!-- Section: Copyright -->
       <section class="p-3 pt-0">
         <div class="row d-flex align-items-center">
           <!-- Grid column -->
           <div class="col-md-7 col-lg-8 text-center text-md-start">
-            <!-- Copyright -->
+            <!--=== Copyright ===-->
             <div class="p-3">
-              © 2020 Copyright:
-              <a class="text-white" href="https://mdbootstrap.com/"
-                 >MDBootstrap.com</a
-                >
+             <p>Realizacja <a class="white-custom-text" href="https://rbcode.pl/" target="_blank" title="Strony internetowe i pozycjonowanie">Rbcode</a> 2021</p>
             </div>
-            <!-- Copyright -->
+            <!--=== Copyright ===-->
           </div>
           <!-- Grid column -->
 
           <!-- Grid column -->
           <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-            <!-- Facebook -->
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fab fa-facebook-f"></i
-              ></a>
 
-            <!-- Twitter -->
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fab fa-twitter"></i
-              ></a>
+            <?php do_action( 'rbcode_social_icons' ); ?>
 
-            <!-- Google -->
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fab fa-google"></i
-              ></a>
-
-            <!-- Instagram -->
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fab fa-instagram"></i
-              ></a>
           </div>
           <!-- Grid column -->
         </div>
