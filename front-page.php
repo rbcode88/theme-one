d5118819745592712f86cedc3fc6750b95eb6ba1
robson1988 <robert.bajoo@gmail.com>
2021-11-01 19:03:16 +0100

    #dom

--- front-page.php
+++ front-page.php
@@ -7,10 +7,10 @@
             <div class="hero d-flex justify-content-center flex-column hero-large-window" data-aos="zoom-out">
                 <a href="<?php echo get_site_url()."/trofea-personalizowane"; ?>">
                         <h2 class="window-text-center">TROFEA<br> PERSONALIZOWANE</h2>
-                        <button type="button" class="btn btn-custom">Zobacz więcej
+                        <div class="window-button-center"><button type="button" class="btn btn-custom">Zobacz więcej
                             <div class="button__horizontal"></div>
 	                        <div class="button__vertical"></div>
-                        </button>
+                        </button></div>
                         <div class="image-nav image-1"></div>
                     </a>
               
@@ -24,10 +24,10 @@
                 <div class="col-lg-6 col-md-12 col-6 p-0 hero-small-window" data-aos="zoom-out" data-aos-delay="100">
                     <a href="<?php echo get_site_url()."/dyplomy-drewniane"; ?>">
                         <h2 class="window-text-center">DYPLOMY</h2>
-                        <button type="button" class="btn btn-custom">Zobacz więcej
+                        <div class="window-button-center"><button type="button" class="btn btn-custom">Zobacz więcej
                             <div class="button__horizontal"></div>
 	                        <div class="button__vertical"></div>
-                        </button>
+                        </button></div>
                         <div class="image-nav image-1"></div>
                     </a>
                 </div>
@@ -35,10 +35,10 @@
                 <div class=" col-lg-6 col-md-12 col-6 p-0 hero-small-window" data-aos="zoom-out" data-aos-delay="200">
                     <a href="<?php echo get_site_url()."/statuetki-szklane"; ?>">
                         <h2 class="window-text-center">SZKŁA</h2>
-                        <button type="button" class="btn btn-custom">Zobacz więcej
+                        <div class="window-button-center"><button type="button" class="btn btn-custom">Zobacz więcej
                             <div class="button__horizontal"></div>
 	                        <div class="button__vertical"></div>
-                        </button>
+                        </button></div>
                         <div class="image-nav image-2"></div>
                     </a>
                 </div>
@@ -47,10 +47,10 @@
                 <div class=" col-lg-6 col-md-12 col-6 p-0 hero-small-window" data-aos="zoom-out" data-aos-delay="300">
                     <a href="<?php echo get_site_url()."/trofea-z-pleksy"; ?>">
                         <h2 class="window-text-center">TROFEA Z PLEKSY</h2>
-                        <button type="button" class="btn btn-custom">Zobacz więcej
+                        <div class="window-button-center"><button type="button" class="btn btn-custom">Zobacz więcej
                             <div class="button__horizontal"></div>
 	                        <div class="button__vertical"></div>
-                        </button>
+                        </button></div>
                         <div class="image-nav image-3"></div>
                     </a>
                 </div>
@@ -58,10 +58,10 @@
                 <div class=" col-lg-6 col-md-12 col-6 p-0 hero-small-window" data-aos="zoom-out" data-aos-delay="400">
                     <a href="<?php echo get_site_url()."/v-line-custom"; ?>">
                         <h2 class="window-text-center">V-LINE CUSTOM</h2>
-                        <button type="button" class="btn btn-custom">Zobacz więcej
+                        <div class="window-button-center"><button type="button" class="btn btn-custom">Zobacz więcej
                             <div class="button__horizontal"></div>
 	                        <div class="button__vertical"></div>
-                        </button>
+                        </button></div>
                         <div class="image-nav image-4"></div>
                     </a>
                 </div>
